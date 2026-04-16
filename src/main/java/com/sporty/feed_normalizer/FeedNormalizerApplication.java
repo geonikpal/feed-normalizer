ba77b167a6e7f0a6815a379c1b4242fc23c5e09a
geonikpal <geonikpal@gmail.com>
2026-04-16 19:27:12 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sporty/feed_normalizer/FeedNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.sporty.feed_normalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedNormalizerApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(FeedNormalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sporty/feed_normalizer/FeedNormalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sporty.feed_normalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedNormalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
